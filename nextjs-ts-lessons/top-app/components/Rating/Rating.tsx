b1c54bad95ee59428d93d3c86e45bcda40f3ff1f
Pavel <pavelgq@yandex.ru>
2021-10-05 22:22:07 +0300

    make simple rating component

--- nextjs-ts-lessons/top-app/components/Rating/Rating.tsx
+++ nextjs-ts-lessons/top-app/components/Rating/Rating.tsx
@@ -4,9 +4,9 @@ import { RatingProps } from './Rating.props';
 import cn from 'classnames';
 import StarIcon from './star.svg';
 
-import styles from './Htag.module.css'
+import styles from './Rating.module.css'
 
-export const Htag = ({isEditable = false, rating, setRating, ...props} : RatingProps) : JSX.Element => {
+export const Rating = ({isEditable = false, rating, setRating, ...props} : RatingProps) : JSX.Element => {
     
     const [ratingArray, setRatingArray] = useState<JSX.Element[]>(new Array(5).fill(<></>));
 
@@ -17,15 +17,18 @@ export const Htag = ({isEditable = false, rating, setRating, ...props} : RatingP
     const constructRating = (currentRating: number) => {
         const newRating = ratingArray.map((r: JSX.Element, i: number) => {
             return (
-                <StarIcon 
-                    
+                <StarIcon className={cn(styles.star, {
+                    [styles.fill]: i < currentRating
+                })}
                 />
             )
         })
+        setRatingArray(newRating);
     }
-    return (
-        <div>
 
+    return (
+        <div {...props}>
+            {ratingArray.map((r, i) => (<span key={i}>{r}</span>))}
         </div>
     )
 }
--- nextjs-ts-lessons/top-app/pages/index.tsx
+++ nextjs-ts-lessons/top-app/pages/index.tsx
@@ -2,12 +2,13 @@ import Head from 'next/head'
 import Image from 'next/image'
 import React from 'react'
 
-import { Htag } from '../components'
+import { Htag, Rating } from '../components'
 
 export default function Home() {
   return (
     <>
-      <Htag tag='h1'>Lalalalal</Htag>
+      <Htag tag='h1'>Title</Htag>
+      <Rating isEditable={true} rating={1}></Rating>
     </>
   )
 }
